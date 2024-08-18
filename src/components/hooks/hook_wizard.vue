bc3e7934819e888b20da29e624a57b7dd5155b30
unknown <amr_elngm6@yahoo.com>
2024-08-18 23:58:05 +0300

    0.1

--- src/components/hooks/hook_wizard.vue
+++ src/components/hooks/hook_wizard.vue
@@ -238,9 +238,9 @@ export default
                     params.append('params[' + k + ']', d)
                 }
                 console.log(activeItem.value)
-                console.log(JSON.stringify(activeItem.value.options))
+                console.log(activeItem.value.options)
                 let type = array.id > 0 ? 'update' : 'create';
-                params.append('params[options]', JSON.stringify(activeItem.value.options))
+                params.append('params[options]', activeItem.value.options)
                 params.append('type', 'Hook.' + type)
                 handleRequest(params, '/api/' + type).then(response => {
                     handleAccess(response)
