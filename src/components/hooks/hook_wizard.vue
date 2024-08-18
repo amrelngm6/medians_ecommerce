404afc1349064303f3938c9f7f52bb43761f71c3
unknown <amr_elngm6@yahoo.com>
2024-08-18 23:47:38 +0300

    0.1

--- src/components/hooks/hook_wizard.vue
+++ src/components/hooks/hook_wizard.vue
@@ -238,7 +238,7 @@ export default
                     params.append('params[' + k + ']', d)
                 }
                 let type = array.id > 0 ? 'update' : 'create';
-                params.append('params[options]', array['options'])
+                params.append('params[options]', JSON.stringify(activeItem.value.options))
                 params.append('type', 'Hook.' + type)
                 handleRequest(params, '/api/' + type).then(response => {
                     handleAccess(response)
