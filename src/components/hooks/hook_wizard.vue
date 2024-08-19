acdc03b5604d516db87f45033cfb4defe8baa2a9
unknown <amr_elngm6@yahoo.com>
2024-08-19 20:08:20 +0300

    0.1

--- src/components/hooks/hook_wizard.vue
+++ src/components/hooks/hook_wizard.vue
@@ -274,7 +274,7 @@ export default
             const handleField = (val, index) => {
                 console.log(val) 
                 console.log(index) 
-                console.log(activeItem.value.field) 
+                console.log(...activeItem.value.field) 
                 if (activeItem.value.field == null) {
                     activeItem.value.field = {}
                     activeItem.value.field[index] = val
@@ -282,7 +282,7 @@ export default
 
                     activeItem.value.field[index] = val
                 }
-                console.log(activeItem.value.field) 
+                console.log(...activeItem.value.field) 
 
             }
             
