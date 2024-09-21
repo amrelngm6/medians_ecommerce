cae94dafa63b327026d9ec7fcf2f869e44390f49
unknown <amr_elngm6@yahoo.com>
2024-09-21 11:57:36 +0300

    1.0 v

--- src/front_assets/js/slide.js
+++ src/front_assets/js/slide.js
@@ -3,6 +3,8 @@ let currentIndexes = [0,0];
 function showSlides() 
 {
     const cont = document.querySelectorAll('.carousel-container')
+
+    console.log(cont)
     
     if (!cont)
         return null;
@@ -23,7 +25,7 @@ function showSlide(container, i)
     const parentId = container.parentElement.getAttribute('id');
 
     const limit = parseInt(container.getAttribute('data-count') ? container.getAttribute('data-count') : 3);
-    const slides = document.querySelectorAll('#'+id+' .slide');
+    const slides = document.querySelectorAll('#'+id+' > div');
     // console.log(slides)
     const start = currentIndexes[i];
     const end = start + limit;
@@ -117,3 +119,67 @@ window.addEventListener('resize', function(e) {
         })
     }
 });
+
+
+// work in progress - needs some refactoring and will drop JQuery i promise :)
+var instance = jQuery(".hs__wrapper");
+$.each( instance, function(key, value) {
+    
+  var arrows = jQuery(instance[key]).find(".arrow"),
+      prevArrow = arrows.filter('.arrow-prev'),
+      nextArrow = arrows.filter('.arrow-next'),
+      box = jQuery(instance[key]).find(".hs"), 
+      x = 0,
+      mx = 0,
+      maxScrollWidth = box[0].scrollWidth - (box[0].clientWidth / 2) - (box.width() / 2);
+
+      jQuery(arrows).on('click', function() {
+      
+    if (jQuery(this).hasClass("arrow-next")) {
+      x = ((box.width() / 2)) + box.scrollLeft() - 10;
+      box.animate({
+        scrollLeft: x,
+      })
+    } else {
+      x = ((box.width() / 2)) - box.scrollLeft() -10;
+      box.animate({
+        scrollLeft: -x,
+      })
+    }
+      
+  });
+    
+  jQuery(box).on({
+    mousemove: function(e) {
+      var mx2 = e.pageX - this.offsetLeft;
+      if(mx) this.scrollLeft = this.sx + mx - mx2;
+    },
+    mousedown: function(e) {
+      this.sx = this.scrollLeft;
+      mx = e.pageX - this.offsetLeft;
+    },
+    scroll: function() {
+      toggleArrows();
+    }
+  });
+
+  jQuery(document).on("mouseup", function(){
+    mx = 0;
+  });
+  
+  function toggleArrows() {
+    if(box.scrollLeft() > maxScrollWidth - 10) {
+        // disable next button when right end has reached 
+        nextArrow.addClass('disabled');
+      } else if(box.scrollLeft() < 10) {
+        // disable prev button when left end has reached 
+        prevArrow.addClass('disabled')
+      } else{
+        // both are enabled
+        nextArrow.removeClass('disabled');
+        prevArrow.removeClass('disabled');
+      }
+  }
+  
+});
+
