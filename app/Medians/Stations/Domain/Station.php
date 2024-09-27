788b5a4cdb9bbd1bcec512239bac082338685779
unknown <amr_elngm6@yahoo.com>
2024-09-27 12:32:47 +0300

    Fixes

--- app/Medians/Stations/Domain/Station.php
+++ app/Medians/Stations/Domain/Station.php
@@ -38,7 +38,7 @@ class Station extends CustomModel
 
 	public function activeItem()
 	{
-		return $this->hasOne(StationMedia::class, 'station_id', 'station_id')->where('start_at', '<', date('H:i:s'))->with('media');	
+		return $this->hasOne(StationMedia::class, 'station_id', 'station_id')->where('start_at', '<', date('H:i:s'))->with('media')->orderBy('start_at', 'DESC');	
 	}
 
 	public function likes()
