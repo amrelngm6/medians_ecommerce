72bd591b1949075d2f6f073411d474a685d2fe58
unknown <amr_elngm6@yahoo.com>
2024-09-28 22:45:12 +0300

    Fixes

--- app/Medians/Stations/Infrastructure/StationRepository.php
+++ app/Medians/Stations/Infrastructure/StationRepository.php
@@ -24,7 +24,7 @@ class StationRepository
 		$now = date('H:i:s');
 		// $now = date('H:i:s', strtotime("+1 Hours"));
 		return Station::with('items')->with(['activeItem'=> function($q) use ($now) {
-			return $q->whereRaw("? BETWEEN `start_at` AND DATE_ADD(`start_at`, INTERVAL `duration` SECOND)", [$now])->orderBy('duration', 'ASC');
+			return $q->whereRaw("? BETWEEN `start_at` AND DATE_ADD(`start_at`, INTERVAL `duration` SECOND)", [$now])->orderBy('duration', 'ASC')->orderBy('start_at', 'DESC');
 		}])->withCount('likes')->find($id);
 
 
