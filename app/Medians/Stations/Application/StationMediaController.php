1e1ae6d54f5b62ab837de5e35a9f708fee610d58
unknown <amr_elngm6@yahoo.com>
2024-09-28 17:37:52 +0300

    Fixes

--- app/Medians/Stations/Application/StationMediaController.php
+++ app/Medians/Stations/Application/StationMediaController.php
@@ -99,9 +99,11 @@ class StationMediaController extends CustomController
 			$getID3 = new getID3;
 			if (substr($params['media_path'], 0, 4) == 'http' ) {
 				
-				$tempFilePath = $_SERVER['DOCUMENT_ROOT'].'/uploads/audio/tmp/'.md5($params['media_path']).'.mp3';
+                $media_path = '/uploads/audio/tmp/'.md5($params['media_path']).'.mp3';
+				$tempFilePath = $_SERVER['DOCUMENT_ROOT'].$media_path;
 				file_put_contents($tempFilePath, fopen($params['media_path'], 'r'));
 				$filePath = $tempFilePath;
+                $params['media_path'] = $media_path;
 			}
 			$fileInfo = $getID3->analyze($filePath);
 
