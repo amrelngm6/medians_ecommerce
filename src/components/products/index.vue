64caf8c5fd5142edaa7af959e5b5e60c0d1ee3e1
unknown <amr_elngm6@yahoo.com>
2024-10-08 01:23:18 +0300

    Media Admin

--- src/components/products/index.vue
+++ src/components/products/index.vue
@@ -4,7 +4,6 @@
         <div  v-if="content " class=" w-full relative">
             
             <div class=" " v-if="showWizard ">
-                <product_wizard :currency="currency" :langs="langs" @callback="()=> {activeItem = null; showWizard = false}" :conf="conf" :auth="auth" :item="activeItem" :path="path+'/'+(activeItem.product_id ? activeItem.product_id : 'new')" :system_setting="system_setting" :setting="setting"  />
             </div>
 
             <main class=" flex-1 overflow-x-hidden overflow-y-auto  w-full relative" v-if="content.items && !showWizard">
@@ -87,14 +86,12 @@ const form_field = defineAsyncComponent(() =>
 );
 
 import tooltip from '@/components/includes/tooltip.vue';
-const product_wizard = defineAsyncComponent(() => import('@/components/products/product_wizard.vue') );
 
 
 export default 
 {
     components:{
         'datatabble': Vue3EasyDataTable,
-        product_wizard,
         delete_icon,
         car_icon,
         form_field,
