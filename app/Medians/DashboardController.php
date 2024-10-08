5564dd4898cfe3506ff6697c857e5d3d8bf49f61
unknown <amr_elngm6@yahoo.com>
2024-10-08 05:16:51 +0300

    Media Admin

--- app/Medians/DashboardController.php
+++ app/Medians/DashboardController.php
@@ -126,7 +126,6 @@ class DashboardController extends CustomController
 
         // $data['top_products'] = $this->ProductRepository;
         $data['top_customers'] = [];
-			
 		$data['video_charts'] = $mediaItemRepo->eventsByDate(['start'=>$this->month_beginning, 'end'=>$this->end])->where('type', 'video')-> selectRaw('Date(created_at) as label, COUNT(*) as y')->having('y', '>', 0)->groupBy('label')->limit('10')->get();
         $data['video_count'] = $mediaItemRepo->eventsByDate(['start'=>$this->start, 'end'=>$this->end])->where('type', 'video')->count();
 
