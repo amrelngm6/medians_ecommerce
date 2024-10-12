3b89d3b2b56438d5636d2932e63e5a611fd2c8e4
unknown <amr_elngm6@yahoo.com>
2024-10-13 02:17:22 +0300

    Rose Color

--- app/Medians/Settings/Application/SiteSettingsController.php
+++ app/Medians/Settings/Application/SiteSettingsController.php
@@ -91,7 +91,7 @@ class SiteSettingsController extends CustomController
 			
 			'layout_options'=> [	
 
-				[ 'key'=> "autoplay_shorts", 'title'=> translate('Autoplay for Shorts'), 'help_text'=>translate('Auto play videos at shorts list page'), 'fillable'=> true, 'column_type'=>'number' ],
+				[ 'key'=> "autoplay_shorts", 'title'=> translate('Autoplay for Shorts'), 'help_text'=>translate('Auto play videos at shorts list page'), 'fillable'=> true, 'column_type'=>'checkbox' ],
 				[ 'key'=> "view_items_limit", 'title'=> translate('Loading items limit'), 'help_text'=>translate('Show x items as first load at frontend pages'), 'fillable'=> true, 'column_type'=>'number' ],
 				// [ 'key'=> "show_newsletter_form", 'title'=> translate('Show newsletter at footer'), 'help_text'=>translate('Show newsletter form at footer to allow users to subscribe'), 'fillable'=> true, 'column_type'=>'checkbox' ],
 			],
