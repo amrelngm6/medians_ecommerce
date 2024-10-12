bf66e41e09de474bdacb07ad0abd24f4b516e46f
unknown <amr_elngm6@yahoo.com>
2024-10-12 23:48:01 +0300

    Rose Color

--- app/Medians/Media/Application/MediaItemController.php
+++ app/Medians/Media/Application/MediaItemController.php
@@ -245,14 +245,14 @@ class MediaItemController extends CustomController
         
         $artistRepo = new \Medians\Customers\Infrastructure\CustomerRepository;
         $query['limit'] = $settings['view_items_limit'] ?? null;
-        $channels = $artistRepo->getWithFilter($query);
+        $artists = $artistRepo->getWithFilter($query);
         
 		try 
         {
             return printResponse(render('views/front/'.($settings['template'] ?? 'default').'/layout.html.twig', [
                 'app' => $this->app,
                 'list' => $list,
-                'channels' => $channels,
+                'artists' => $artists,
                 'genres' => $this->categoryRepo->getGenres(),
                 'layout' => 'discover'
             ], 'output'));
