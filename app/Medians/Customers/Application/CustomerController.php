a665b39112488a56010905f99b2b8a5d29d394c9
unknown <amr_elngm6@yahoo.com>
2024-10-12 09:10:36 +0300

    Rose Color

--- app/Medians/Customers/Application/CustomerController.php
+++ app/Medians/Customers/Application/CustomerController.php
@@ -168,7 +168,7 @@ class CustomerController extends CustomController
             return printResponse(render('views/front/'.($settings['template'] ?? 'default').'/layout.html.twig', [
                 'app' => $this->app,
                 'channels' => $this->repo->get(),
-                'layout' => 'channels'
+                'layout' => 'arists'
             ], 'output'));
             
 		} catch (\Exception $e) {
