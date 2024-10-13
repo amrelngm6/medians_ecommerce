01c076592e37f101bcca8ac97436c98c26e3a4a4
unknown <amr_elngm6@yahoo.com>
2024-10-13 09:19:17 +0300

    Rose Color

--- app/config/route.php
+++ app/config/route.php
@@ -32,6 +32,7 @@ RouteHandler::get('/switch-lang/(:all)', \Medians\DashboardController::class.'@s
 RouteHandler::get('/', \Medians\Pages\Application\PageController::class.'@homepage');
 RouteHandler::get('/dashboard/login', \Medians\Auth\Application\AuthService::class.'@loginPage');
 RouteHandler::get('/google_login_redirect', \Medians\Auth\Application\CustomerAuthService::class.'@verifyLoginWithGoogle');
+RouteHandler::get('/facebook_login_redirect', \Medians\Auth\Application\CustomerAuthService::class.'@verifyLoginWithFacebook');
 RouteHandler::get('/activate-account/(:all)', \Medians\Auth\Application\CustomerAuthService::class.'@activate');
 RouteHandler::get('/reset-password', \Medians\Auth\Application\AuthService::class.'@resetPasswordPage');
 RouteHandler::get('/reset-password-code', \Medians\Auth\Application\AuthService::class.'@resetPasswordCodePage');
