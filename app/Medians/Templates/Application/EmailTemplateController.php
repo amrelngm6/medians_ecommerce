4c4bbbbc852c16ebe771f8d35a5c58ebe33028c3
unknown <amr_elngm6@yahoo.com>
2024-10-15 06:04:21 +0300

    Handle RTL Styles

--- app/Medians/Templates/Application/EmailTemplateController.php
+++ app/Medians/Templates/Application/EmailTemplateController.php
@@ -35,7 +35,7 @@ class EmailTemplateController extends CustomController
             [ 'value'=> "template_id", 'text'=> "#"],
             [ 'value'=> "title", 'text'=> translate('Title'), 'sortable'=> true ],
             [ 'value'=> "status", 'text'=> translate('Status'), 'sortable'=> true ],
-			[ 'value'=> 'text', 'text'=>translate('Edit')],
+			[ 'value'=> 'edit', 'text'=>translate('Edit')],
 			['value'=>'delete', 'text'=>translate('Delete')],
         ];
 	}
