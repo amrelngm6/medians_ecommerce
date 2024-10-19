fdd0c265c4d8461ae8f7a6d2d55154c94c6e2635
unknown <amr_elngm6@yahoo.com>
2024-10-20 02:22:04 +0300

    Handle RTL Styles

--- app/Medians/Packages/Application/PackageSubscriptionController.php
+++ app/Medians/Packages/Application/PackageSubscriptionController.php
@@ -336,10 +336,19 @@ class PackageSubscriptionController extends CustomController
 				throw new \Exception(translate('Cancel your current subscription first'), 1);
 			}
 			
+			if ($customer->subscription->is_valid && !$customer->subscription->is_paid )
+			{
+				throw new \Exception(translate('Cancel your current subscription first'), 1);
+			}
+			
 			if ($customer->subscription->is_valid && !$customer->subscription->is_paid && !$item->is_paid )
 			{
 				throw new \Exception(translate('Your current package and this one are free'), 1);
 			}
+
+			print_r($customer);
+			print_r($item);
+
 		}
 
 	}
