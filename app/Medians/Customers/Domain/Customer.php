c91faab5852c9da454c7d45c84dc04a13295c629
unknown <amr_elngm6@yahoo.com>
2024-10-20 02:47:17 +0300

    Handle RTL Styles

--- app/Medians/Customers/Domain/Customer.php
+++ app/Medians/Customers/Domain/Customer.php
@@ -86,7 +86,7 @@ class Customer extends CustomModel
 
 	public function subscription() 
 	{
-		return $this->hasOne(PackageSubscription::class , 'customer_id', 'customer_id')->orderBy('end_date', 'DESC');	
+		return $this->hasOne(PackageSubscription::class , 'customer_id', 'customer_id')->where('status','active')->orderBy('end_date', 'DESC');	
 	}
 
 	public function followers() 
