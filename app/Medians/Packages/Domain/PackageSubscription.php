94313180310a23f723cd2294047213ab1483ca95
unknown <amr_elngm6@yahoo.com>
2024-10-20 00:49:52 +0300

    Handle RTL Styles

--- app/Medians/Packages/Domain/PackageSubscription.php
+++ app/Medians/Packages/Domain/PackageSubscription.php
@@ -39,7 +39,7 @@ class PackageSubscription extends CustomModel
     
 	public function getIsValidAttribute()
 	{
-		return (strtotime(date("Y-m-d")) <= strtotime($this->end_date)) ? true : false;
+		return (strtotime(date("Y-m-d")) <= strtotime($this->end_date)) ? ($this->status == 'active') : false;
 	}
     
 	public function getFieldAttribute()
