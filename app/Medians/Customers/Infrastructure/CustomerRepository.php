153b9754777e7dfb5d574b407ec02ae50ab6753a
unknown <amr_elngm6@yahoo.com>
2024-10-20 12:10:26 +0300

    Handle Dashboard

--- app/Medians/Customers/Infrastructure/CustomerRepository.php
+++ app/Medians/Customers/Infrastructure/CustomerRepository.php
@@ -46,7 +46,7 @@ class CustomerRepository
 		return Customer::with(['subscription'=> function($q){
 			return $q->with('package');
 		}])
-		->with('subscriptions','videos' ,'audio_items' ,'audiobooks','stations' ,'channels','short_videos','playlists' ,'videos' )->get();
+		->withCount('subscriptions','videos' ,'audio_items' ,'audiobooks','stations' ,'channels','short_videos','playlists' ,'videos' )->get();
 	}
 
 	public function findByEmail($email)
